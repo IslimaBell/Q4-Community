4edbb577e1e43d7ac14c1267258449bd1413e174
MUKILTEO\1710152 <Riley05Park@gmail.com>
2022-03-17 13:25:32 -0700

    Worked more on the first iteration of the map

---
+++ TheCommunity/Assets/Riley/Scripts/BasicMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float accel = 8;
+    public float JumpHeight;
+    private Rigidbody2D rb2;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+
+        //Move right
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = false;
+            rb2.AddForce(new Vector2(accel, 0));
+        }
+
+        //Move left
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+            rb2.AddForce(new Vector2(-accel, 0));
+        }
+
+        //JUMP
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2.AddForce(new Vector2(0, JumpHeight));
+        }
+
+
+
+    }
+}
