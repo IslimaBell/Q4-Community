89c87ecb2e0992746e1a58fbd1196efde7bbb23c
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-03-23 13:34:30 -0700

    AI line of sight started

---
+++ TheCommunity/Assets/Andrea/Scripts/AIEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+        //print("distToPlayer:" + distToPlayer);
+
+        if(distToPlayer > agroRange)
+        {
+            // chasing
+            ChasePlayer();
+        }
+        else
+        {
+            //stop chasing
+            StopChasing();
+        }
+    }
+
+    private void StopChasing()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+       
+    }
+
+    private void ChasePlayer()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
