115c3a3e0ff053d4705cf68e29c515837260465b
MUKILTEO\1709546 <Isaiah2124.ib@gmail.com>
2022-04-01 12:02:09 -0700

    Main Menu Templete, Scene Interconection, Cutscene 1 temp

---
+++ TheCommunity/Assets/Isaiah/Script/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
