adbfd8bacb79d3fec2ae516d21c583f3eb87deb5
MUKILTEO\1710152 <Riley05Park@gmail.com>
2022-04-11 12:53:21 -0700

    updated tutorial level to be a better tutorial level

---
+++ TheCommunity/Assets/Riley/Scripts/BasicMoveEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMoveEvent : MonoBehaviour
+{
+    public GameObject actor;
+    public float XChange;
+    public float YChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("EVENT TRIGGERED");
+        actor.GetComponent<Rigidbody2D>().velocity = new Vector2(XChange, YChange);
+        //actor.GetComponent<Rigidbody2D>().velocity = new Vector2(actor.GetComponent<Rigidbody2D>().velocity.x + XChange, actor.GetComponent<Rigidbody2D>().velocity.y + YChange);
+
+    }
+
+
+}
