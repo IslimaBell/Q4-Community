80a312d1def71e997dc90349c4f60158e339381d
MUKILTEO\1703682 <1703682@Mukilteo.wednet.edu>
2022-04-25 11:44:20 -0700

    fixed bug

--- TheCommunity/Assets/Andrea/AI.cs
+++ TheCommunity/Assets/Andrea/AI.cs
@@ -6,13 +6,13 @@ using CodeMonkey.Utils;
 public class AI : MonoBehaviour
 {
 
-    private EnemyPathfindingMovement pathfindingMovement;
+    //private EnemyPathfindingMovement pathfindingMovement;
     private Vector3 startingPosition;
     private Vector3 roamPosition;
 
     private void Awake()
     {
-        pathfindingMovement = GetComponent < EnemyPathfindingMovement();
+        //pathfindingMovement = GetComponent < EnemyPathfindingMovement();
     }
     // Start is called before the first frame update
     void Start()
@@ -25,15 +25,15 @@ public class AI : MonoBehaviour
 
     private Vector3 GetRoamingPosition()
     {
-        startingPosition + UtilsClass.GetRandomDir() * Random.range(10f, 70f);
+       return startingPosition + UtilsClass.GetRandomDir() * Random.Range(10f, 70f);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        pathfindingMovement.MoveTo(roamPosition);
+        //pathfindingMovement.MoveTo(roamPosition);
         float reachedPositionDistance = 1f;
-        if (Vector3.Distance(transform.position,roamPosition) < reachedPositionDistance{
+        if (Vector3.Distance(transform.position,roamPosition) < reachedPositionDistance){
 
         }
     } 
